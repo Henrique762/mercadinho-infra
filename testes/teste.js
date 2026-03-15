1b3b6bd1fab207aaba26c6a9a582dc3dc1a64656
Henrique Gil <henriquegil123@gmail.com>
2026-03-15 20:47:44 -0300

    remove lambda

--- testes/teste.js
+++ testes/teste.js
@@ -4,12 +4,12 @@ import { check, sleep } from 'k6';
 // 1. Configuração do Teste de Carga (Options)
 export const options = {
   stages: [
-    { duration: '30s', target: 100 }, // Sobe para 20 usuários simultâneos em 30s
-    { duration: '3m', target: 100 },  // Mantém 20 usuários por 1 minuto
-    { duration: '20s', target: 0 },  // Desce para 0 (ramping-down)
+    { duration: '20s', target: 30 }, // Sobe para 30 usuários simultâneos em 20s
+    { duration: '20s', target: 30 },  // Mantém 5 usuários por 20 segundos
+    { duration: '10s', target: 0 },  // Desce para 0 (ramping-down)
   ],
   thresholds: {
-    http_req_failed: ['rate<0.01'], // O teste falha se mais de 1% das requisições derem erro
+    http_req_failed: ['rate<0.15'], // O teste falha se mais de 1% das requisições derem erro
     http_req_duration: ['p(95)<500'], // 95% das requisições devem ser abaixo de 500ms
   },
 };
@@ -22,9 +22,9 @@ export default function () {
   const uniqueID = Date.now() + Math.random();
   
   const payload = JSON.stringify({
-    nome: `GUIGUI_${uniqueID}`,
+    nome: `${uniqueID}`,
     cnpj: `${Math.floor(Math.random() * 100000000000000)}`, // CNPJ randômico simples
-    email: `gui_${uniqueID}@gmail.com`,
+    email: `${uniqueID}@gmail.com`,
     senha: "12345aH@",
     celular: "+5511962968213"
   });
@@ -43,6 +43,4 @@ export default function () {
     'status é 201 ou 200': (r) => r.status === 201 || r.status === 200,
     'tempo de resposta < 500ms': (r) => r.timings.duration < 500,
   });
-
-  // Aguarda 1 segundo entre uma requisição e outra por cada Usuário Virtual (VU)
 }
